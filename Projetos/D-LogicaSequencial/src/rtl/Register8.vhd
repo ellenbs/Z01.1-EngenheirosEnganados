e26e257c39460f342101a830a73c6ded79f5f084
Gabriella <gabriellaec@al.insper.edu.br>
2020-04-03 09:51:07 -0300

    BinaryDigit

--- Projetos/D-LogicaSequencial/src/rtl/Register8.vhd
+++ Projetos/D-LogicaSequencial/src/rtl/Register8.vhd
@@ -26,6 +26,14 @@ architecture arch of Register8 is
 	end component;
 
 begin
+	r1 : BinaryDigit port map (clock, input(0), load, output(0));
+	r2 : BinaryDigit port map (clock, input(1), load, output(1));
+	r3 : BinaryDigit port map (clock, input(2), load, output(2));
+	r4 : BinaryDigit port map (clock, input(3), load, output(3));
+	r5 : BinaryDigit port map (clock, input(4), load, output(4));
+	r6 : BinaryDigit port map (clock, input(5), load, output(5));
+	r7 : BinaryDigit port map (clock, input(6), load, output(6));
+	r8 : BinaryDigit port map (clock, input(7), load, output(7));
 
 
 end architecture;
