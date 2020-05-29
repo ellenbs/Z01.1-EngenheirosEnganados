ba0d00d13270e9d5031b695d5dc5e318f0779ae9
Ellen <ellenbs@al.insper.edu.br>
2020-05-29 09:37:51 -0300

    Detalhes

--- Projetos/G-Assembler/Assembler/src/main/java/assembler/Code.java
+++ Projetos/G-Assembler/Assembler/src/main/java/assembler/Code.java
@@ -17,40 +17,6 @@ public class Code {
      */
     public static String dest(String[] mnemnonic) {
         switch (mnemnonic[0]){
-            case "movw %A, %D"  :
-            case "incw %D"   :
-            case "movw (%A), %D"   :
-            case "addw (%A) %D %D"   :
-            case "decw %D"   :
-            case "notw %D"   :
-            case "negw %D"   :
-            case "andw (%A) %D %D"   :
-            case "orw (%A) %D %D"   :
-                return "0010";
-
-            case "subw %D (%A) %A"   :
-            case "rsubw %D (%A) %A"   :
-            case "decw %A"   :
-            case "notw %A"   :
-            case "negw %A"   :
-            case "andw %D %A %A"   :
-            case "orw %D %A %A"   :
-                return "0001";
-
-            case "movw %A, (%A)"   :
-            case "incw (%A)"   :
-                return "0100";
-
-            case "movw %A, %D, (%A)"   :
-                return "0110";
-
-            case "nop"   :
-            case "jmp"   :
-            case "je"   :
-            case "jne"   :
-            case "jg"   :
-            case "jge"   :
-            case "jl"   :
             default    : return "0000";
         }
     }
@@ -62,45 +28,6 @@ public class Code {
      */
     public static String comp(String[] mnemnonic) {
         switch (mnemnonic[0]){
-            case "movw %A, %D"  : return "000110000";
-            case "movw %D %A"  : return "000001100";
-            case "movw %D (%A)"  : return "000001100";
-            case "movw (%A), %A"  : return "001110000";
-            case "movw %A, (%A)"  : return "000110000";
-
-            case "addw %A   %D %D"  : return "000000010";
-            case "addw (%A) %D %D"  : return "001000010";
-            case "addw $1 (%A) %D"  : return "001110111";
-
-            case "incw %A"  : return "000110111";
-            case "incw %D"  : return "000011111";
-            case "incw (%A)"  : return "001110111";
-
-            case "movw (%A) %D"  : return "001110000";
-            case "subw %D (%A) %A"  : return "001010011";
-
-            case "rsubw %D (%A) %A"  : return "001000111";
-            case "decw %A"  : return "000110010";
-            case "decw %D"  : return "000001110";
-            case "notw %A"  : return "000110001";
-            case "notw %D"  : return "000001101";
-            case "negw %A"  : return "000110011";
-            case "negw %D"  : return "000001111";
-            case "andw (%A) %D %D"  : return "001000000";
-            case "andw %D %A %A"  : return "000000000";
-            case "orw (%A) %D %D"  : return "001010101";
-            case "orw %D %A %A"  : return "000010101";
-            case "subw (%A), $1, %A"  : return "001110010";
-
-            case "nop"   :
-            case "jmp"   :
-            case "je"   :
-            case "jne"   :
-            case "jg"   :
-            case "jge"   :
-            case "jl"   :
-                return "000001100";
-
             default    : return "0000";
         }
     }
@@ -129,21 +56,21 @@ public class Code {
      * @return Valor em binário (String de 15 bits) representado com 0s e 1s.
      */
     public static String toBinary(String symbol) {
-       if(symbol=="0"){
+       if(symbol.equals("0")){
            return "0000000000000000";
-       }else if(symbol=="1"){
+       }else if(symbol.equals("1")){
            return "0000000000000001";
-       }else if(symbol=="10"){
+       }else if(symbol.equals("10")){
            return "0000000000001010";
-       }else if(symbol=="100"){
+       }else if(symbol.equals("100")){
            return "0000000001100100";
-       }else if(symbol=="1000"){
+       }else if(symbol.equals("1000")){
            return "0000001111101000";
-       }else if(symbol=="21845"){
+       }else if(symbol.equals("21845")){
            return "0101010101010101";
-       }else if(symbol=="32767"){
+       }else if(symbol.equals("32767")){
            return "0111111111111111";
-       }else if(symbol=="32767"){
+       }else if(symbol.equals("32767")){
            return "0111111111111111";
        }else{
            return "1111111111111111";
